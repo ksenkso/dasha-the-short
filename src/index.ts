bf28de8ae894678dcd24fad8e8a34f2577531b6e
ksenkso <yazunya@yandex.ru>
2023-04-15 20:32:51 +0300

    add server to interact with the bot

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,39 @@ import { message } from 'telegraf/filters';
 import { downloadShort } from './download-short';
 import { onYoutubeShortLink } from './setup';
 import { logger } from './logger';
+import fastify from 'fastify';
+import cors from '@fastify/cors';
 
 dotenv.config();
 
 export const dasha = new Telegraf(process.env.BOT_TOKEN as string);
+const server = fastify({ logger: false });
+
+server.register(cors);
+
+const CHATS = {
+  genshin_conference: -1001536083195,
+  test: -987219814,
+} as const;
+
+server.post<{ Body: { chatAlias: keyof typeof CHATS; text: string } }>(
+  '/sendMessage',
+  (request, reply) => {
+    const { chatAlias, text } = request.body;
+    const chatId = CHATS[chatAlias];
+
+    dasha.telegram
+      .sendMessage(chatId, text)
+      .then(() => {
+        reply.status(201).send();
+      })
+      .catch(err => {
+        reply.status(500).send(err);
+      });
+  },
+);
+
+server.listen(5000);
 
 dasha.on(message('entities'), ctx => {
   logger.log({
@@ -15,6 +44,7 @@ dasha.on(message('entities'), ctx => {
     label: 'index',
     message: 'got a message',
   });
+  logger.info(ctx.message.chat.id);
   onYoutubeShortLink(ctx, url => {
     return downloadShort(ctx, url);
   });
